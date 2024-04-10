9ba52abf8d5d58a91cf745798e0ff92ca41b2d78
astral4 <88992929+astral4@users.noreply.github.com>
2024-04-09 22:36:46 -0400

    add dataset struct + file imports

---
+++ src/data.rs
@@ -0,0 +1,82 @@
+use anyhow::Result;
+use burn::data::dataset::Dataset;
+use burn::tensor::{backend::Backend, Data, ElementConversion, Shape, Tensor};
+use std::{fs::read, iter::zip, path::Path};
+use tap::Pipe;
+
+const IMAGE_WIDTH: usize = 28;
+const IMAGE_HEIGHT: usize = 28;
+
+#[derive(Debug)]
+pub struct MnistDataset<B: Backend> {
+    inner: Vec<MnistItem<B>>,
+}
+
+impl<B: Backend> MnistDataset<B> {
+    fn from_idx_files<P1: AsRef<Path>, P2: AsRef<Path>>(
+        images_path: P1,
+        labels_path: P2,
+        device: &B::Device,
+    ) -> Result<Self> {
+        let images_buf = read(images_path)?;
+        let num_images = <[u8; 4]>::try_from(&images_buf[4..7])?.pipe(u32::from_be_bytes);
+
+        let mut labels_buf = read(labels_path)?;
+        let num_labels = <[u8; 4]>::try_from(&labels_buf[4..7])?.pipe(u32::from_be_bytes);
+
+        assert_eq!(
+            num_images, num_labels,
+            "Every image must have exactly one corresponding label"
+        );
+
+        let mut dataset = Vec::with_capacity(num_images as usize);
+
+        for (image, label) in zip(
+            images_buf[16..].chunks_exact(IMAGE_WIDTH * IMAGE_HEIGHT),
+            labels_buf.drain(8..),
+        ) {
+            let tensor_data = Data {
+                // convert pixel values to f32
+                value: image
+                    .iter()
+                    .map(|n| f32::from(*n))
+                    .map(ElementConversion::elem)
+                    .collect(),
+                shape: Shape {
+                    dims: [1, IMAGE_WIDTH, IMAGE_HEIGHT],
+                },
+            };
+
+            let mut tensor = Tensor::<B, 3>::from_data(tensor_data, device);
+
+            // Normalizes pixel values so that every pixel is in the range [0, 1]
+            // and, across the entire dataset, the mean is 0 and the std dev is 1.
+            // According to the PyTorch MNIST example[1], after converting to the range, the mean is 0.1307 and the std dev is 0.3081.
+            // [1]: https://github.com/pytorch/examples/blob/7df10c2a8606d26a251f322b62c6c4de501b6519/mnist/main.py#L122
+            tensor = ((tensor / 255) - 0.1307) / 0.3081;
+
+            dataset.push(MnistItem {
+                image: tensor,
+                label: i32::from(label),
+            });
+        }
+
+        Ok(Self { inner: dataset })
+    }
+}
+
+impl<B: Backend> Dataset<MnistItem<B>> for MnistDataset<B> {
+    fn get(&self, index: usize) -> Option<MnistItem<B>> {
+        self.inner.get(index).cloned()
+    }
+
+    fn len(&self) -> usize {
+        self.inner.len()
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct MnistItem<B: Backend> {
+    image: Tensor<B, 3>,
+    label: i32,
+}
