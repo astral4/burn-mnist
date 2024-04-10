a5c79c70ed70fe06b14a4c5f267176e9585893cf
astral4 <88992929+astral4@users.noreply.github.com>
2024-04-10 15:40:31 -0400

    rename targets to labels

--- src/model.rs
+++ src/model.rs
@@ -69,12 +69,12 @@ impl<B: Backend> Model<B> {
     pub fn forward_classification(
         &self,
         images: Tensor<B, 3>,
-        targets: Tensor<B, 1, Int>,
+        labels: Tensor<B, 1, Int>,
     ) -> ClassificationOutput<B> {
         let output = self.forward(images);
         let loss =
-            CrossEntropyLoss::new(None, &output.device()).forward(output.clone(), targets.clone());
+            CrossEntropyLoss::new(None, &output.device()).forward(output.clone(), labels.clone());
 
-        ClassificationOutput::new(loss, output, targets)
+        ClassificationOutput::new(loss, output, labels)
     }
 }
