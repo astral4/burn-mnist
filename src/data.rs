6bfaad84f926bc944024a7e4ddcc48574f504e7a
astral4 <88992929+astral4@users.noreply.github.com>
2024-04-09 23:28:13 -0400

    refactor tensor conversion

--- src/data.rs
+++ src/data.rs
@@ -10,15 +10,14 @@ const IMAGE_WIDTH: usize = 28;
 const IMAGE_HEIGHT: usize = 28;
 
 #[derive(Debug)]
-pub struct MnistDataset<B: Backend> {
-    inner: Vec<MnistItem<B>>,
+pub struct MnistDataset {
+    inner: Vec<MnistItem>,
 }
 
-impl<B: Backend> MnistDataset<B> {
+impl MnistDataset {
     fn from_idx_files<P1: AsRef<Path>, P2: AsRef<Path>>(
         images_path: P1,
         labels_path: P2,
-        device: &B::Device,
     ) -> Result<Self> {
         let images_buf = read(images_path)?;
         let num_images = <[u8; 4]>::try_from(&images_buf[4..7])?.pipe(u32::from_be_bytes);
@@ -37,36 +36,19 @@ impl<B: Backend> MnistDataset<B> {
             images_buf[16..].chunks_exact(IMAGE_WIDTH * IMAGE_HEIGHT),
             labels_buf.drain(8..),
         ) {
-            // convert image to tensor
-            let image_data = Data {
-                // convert pixel values to f32
-                value: image
-                    .iter()
-                    .map(|n| f32::from(*n))
-                    .map(ElementConversion::elem)
-                    .collect(),
-                shape: Shape {
-                    dims: [1, IMAGE_WIDTH, IMAGE_HEIGHT],
-                },
-            };
-            let mut image_tensor = Tensor::from_data(image_data, device);
-
             // We normalize pixel values so that every pixel is in the range [0, 1]
             // and, across the entire dataset, the mean is 0 and the std dev is 1.
             // According to the PyTorch MNIST example[1], after converting to the range, the mean is 0.1307 and the std dev is 0.3081.
             // [1]: https://github.com/pytorch/examples/blob/7df10c2a8606d26a251f322b62c6c4de501b6519/mnist/main.py#L122
-            image_tensor = ((image_tensor / 255) - 0.1307) / 0.3081;
-
-            // convert label to tensor
-            let label_data = Data {
-                value: vec![i16::from(label).elem()],
-                shape: Shape { dims: [1] },
-            };
-            let label_tensor = Tensor::from_data(label_data, device);
+            let image_data = image
+                .iter()
+                .map(|n| f32::from(*n))
+                .map(|n| ((n / 255.) - 0.1307) / 0.3081)
+                .collect();
 
             dataset.push(MnistItem {
-                image: image_tensor,
-                label: label_tensor,
+                image: image_data,
+                label: i16::from(label),
             });
         }
 
@@ -74,8 +56,8 @@ impl<B: Backend> MnistDataset<B> {
     }
 }
 
-impl<B: Backend> Dataset<MnistItem<B>> for MnistDataset<B> {
-    fn get(&self, index: usize) -> Option<MnistItem<B>> {
+impl Dataset<MnistItem> for MnistDataset {
+    fn get(&self, index: usize) -> Option<MnistItem> {
         self.inner.get(index).cloned()
     }
 
@@ -85,9 +67,9 @@ impl<B: Backend> Dataset<MnistItem<B>> for MnistDataset<B> {
 }
 
 #[derive(Clone, Debug)]
-pub struct MnistItem<B: Backend> {
-    image: Tensor<B, 3>,
-    label: Tensor<B, 1, Int>,
+pub struct MnistItem {
+    image: Vec<f32>,
+    label: i16,
 }
 
 #[derive(Debug)]
@@ -101,10 +83,34 @@ impl<B: Backend> MnistBatcher<B> {
     }
 }
 
-impl<B: Backend> Batcher<MnistItem<B>, MnistBatch<B>> for MnistBatcher<B> {
-    fn batch(&self, items: Vec<MnistItem<B>>) -> MnistBatch<B> {
-        let images = items.iter().map(|item| item.image.clone()).collect();
-        let labels = items.iter().map(|item| item.label.clone()).collect();
+impl<B: Backend> Batcher<MnistItem, MnistBatch<B>> for MnistBatcher<B> {
+    fn batch(&self, items: Vec<MnistItem>) -> MnistBatch<B> {
+        let mut images = Vec::with_capacity(items.len());
+        let mut labels = Vec::with_capacity(items.len());
+
+        for item in items {
+            // convert image to tensor
+            let image_data = Data {
+                value: item
+                    .image
+                    .into_iter()
+                    .map(ElementConversion::elem)
+                    .collect(),
+                shape: Shape {
+                    dims: [1, IMAGE_WIDTH, IMAGE_HEIGHT],
+                },
+            };
+            let image_tensor = Tensor::from_data(image_data, &self.device);
+            images.push(image_tensor);
+
+            // convert label to tensor
+            let label_data = Data {
+                value: vec![item.label.elem()],
+                shape: Shape { dims: [1] },
+            };
+            let label_tensor = Tensor::from_data(label_data, &self.device);
+            labels.push(label_tensor);
+        }
 
         MnistBatch {
             images: Tensor::cat(images, 0),
